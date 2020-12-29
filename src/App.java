b121fa0f9c1559c53b7dcc871ec0a2345b5fd0bd
James Churchill <james@smashdev.com>
2020-12-29 08:31:48 -0800

    Update App class

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Something else...");
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("James");
+        names.add("Kiel");
+        names.add("Irina");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
