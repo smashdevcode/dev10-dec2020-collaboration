d35f51fa1d73e325834bdcce91dc9c29d23a5b22
Kiel Donelson <38928487+kdonelson@users.noreply.github.com>
2020-12-29 11:30:32 -0500

    Update App.java
    wonderful stuff

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Something else...");
+        System.out.println("More changes to cause merge conflicts");
+        System.out.println("Breaking James's app...whoops");
     }
 }
